da1ae3a46f5cff7369d17a5410ce23a46d4af37c
MaliceKy <129139550+MaliceKy@users.noreply.github.com>
2023-05-27 07:05:39 -0400

    home page created (Work in progress)

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/HomePage.css';
+
+function HomePage() {
+  return (
+    <div>
+      <h1 className="Title">Welcome to Vaultwise!</h1>
+      <p>Your personal expense tracking assistant.</p>
+    </div>
+  );
+}
+
+export default HomePage;
