39ae57a90c969b80ca629450bb603ab7b7ebbc66
MaliceKy <129139550+MaliceKy@users.noreply.github.com>
2023-06-21 18:42:11 -0400

    Fixed navmenu from scrolling to far and desc width

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 function MyNavbar() {
     const [open, setOpen] = useState(false);
-    const navigate = useNavigate(); // useNavigate hook
-    const location = useLocation(); // useLocation hook
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        const handleHashChange = () => {
+            const navbarHeight = 70; // Set this to the height of your navbar
+            const element = document.getElementById(window.location.hash.substring(1));
+            if (element) {
+                window.scrollTo({
+                    top: element.getBoundingClientRect().top + window.pageYOffset - navbarHeight,
+                    behavior: 'smooth'
+                });
+            }
+        };
+
+        // Add the event listener
+        window.addEventListener('hashchange', handleHashChange);
+
+        // Clean up the event listener when the component unmounts
+        return () => window.removeEventListener('hashchange', handleHashChange);
+    }, []);
 
     return (
         <Navbar className="navbar" expand="md" fixed="top" expanded={open}>
@@ -15,32 +34,28 @@ function MyNavbar() {
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="links">
                     {location.pathname === "/Sign-in" ? (
-                        // Different navigation for sign-in page
                         <>
                             <Nav.Link onClick={() => { setOpen(false); navigate('/'); }} id="nav-home">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/Sign-up" onClick={()=> setOpen(false)} id="nav-sign-up">Sign up</Nav.Link>
+                            <Nav.Link as={Link} to="/Sign-up" onClick={() => setOpen(false)} id="nav-sign-up">Sign up</Nav.Link>
                         </>
                     ) : location.pathname === "/Sign-up" ? (
-                            // Different navigation for sign-in page
-                            <>
-                                <Nav.Link onClick={() => { setOpen(false); navigate('/'); }} id="nav-home">Home</Nav.Link>
-                                <Nav.Link as={Link} to="/Sign-in" onClick={()=> setOpen(false)} id="nav-sign-in">Sign in</Nav.Link>
-                            </>
+                        <>
+                            <Nav.Link onClick={() => { setOpen(false); navigate('/'); }} id="nav-home">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/Sign-in" onClick={() => setOpen(false)} id="nav-sign-in">Sign in</Nav.Link>
+                        </>
                     ) : (
-                        
-                        // Default navigation
                         <>
                             <Nav.Link onClick={() => { setOpen(false); navigate('/'); window.location.hash = 'Home'; }} id="nav-home">Home</Nav.Link>
                             <Nav.Link onClick={() => { setOpen(false); navigate('/'); window.location.hash = 'About'; }} id="nav-about">About</Nav.Link>
                             <Nav.Link onClick={() => { setOpen(false); navigate('/'); window.location.hash = 'Getting-Started'; }} id="nav-getting-started">Getting Started</Nav.Link>
                             <Nav.Link onClick={() => { setOpen(false); navigate('/'); window.location.hash = 'Contact'; }} id="nav-contact-us">Contact Us</Nav.Link>
-                            <Nav.Link as={Link} to="/Sign-in" onClick={()=> setOpen(false)} id="nav-sign-in">Sign in</Nav.Link>
+                            <Nav.Link as={Link} to="/Sign-in" onClick={() => setOpen(false)} id="nav-sign-in">Sign in</Nav.Link>
                         </>
                     )}
                 </Nav>
             </Navbar.Collapse>
 
-            <div onClick={()=> setOpen(!open)} className={`navbar-toggle ${open ? 'open' : ''}`} aria-controls="responsive-navbar-nav">
+            <div onClick={() => setOpen(!open)} className={`navbar-toggle ${open ? 'open' : ''}`} aria-controls="responsive-navbar-nav">
                 <div></div>
                 <div></div>
                 <div></div>
@@ -50,5 +65,6 @@ function MyNavbar() {
 }
 
 export default MyNavbar;
+
 
 
