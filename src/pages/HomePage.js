be422fe9261b0eb76cda743b8db5ec96a5efff81
MaliceKy <Kyle.Malice2001@gmail.com>
2023-07-18 11:19:41 -0400

    changed landscape on iphone, and padding issues.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 // HomePage.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Home-Homepage.css';
 import '../styles/About-Homepage.css'
 import '../styles/GettingStarted-Homepage.css'
@@ -8,13 +9,12 @@ import '../styles/Contact-Homepage.css'
 function HomePage() {
   return (
     <div className="home-container">
-
       <div className="Home" id="Home">
         <div className="Home-Content">
           <div className="Home-Info">
             <h1 className="Title">VaultWise</h1>
             <p className="Desc">Your personal expense tracking assistant.</p>
-            <a className="Sign-in" href="/Sign-up">Sign up</a>
+            <Link className="Sign-in" to="/Sign-up">Sign up</Link>
           </div>
           <div className="wave-container">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#7936ed" fillOpacity="1" d="M0,96L48,106.7C96,117,192,139,288,138.7C384,139,480,117,576,133.3C672,149,768,203,864,202.7C960,203,1056,149,1152,122.7C1248,96,1344,96,1392,96L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
