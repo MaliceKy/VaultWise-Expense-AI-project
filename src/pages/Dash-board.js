9e819bfd90d56ffe748ed3480141f5d0a60d0200
MaliceKy <129139550+MaliceKy@users.noreply.github.com>
2023-07-19 00:33:48 -0400

    Chat GPT 3.5 turbo is now intergrated into site

--- src/pages/Dash-board.js
+++ src/pages/Dash-board.js
@@ -5,6 +5,9 @@ import { Date } from 'core-js';
 import '../styles/Dash-board.css';
 import { ReactComponent as PlaneIcon } from '../assets/images/paper-plane-svgrepo-com.svg';
 
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator} from '@chatscope/chat-ui-kit-react'
+
 // Dashboard Component
 const Dashboard = () => {
 
@@ -13,10 +16,87 @@ const Dashboard = () => {
   const [totalOutcome, setTotalOutcome] = useState(0);
   const [transactions, setTransactions] = useState([]);
   const [chartData, setChartData] = useState([]);
-  const [messages, setMessages] = useState([]);
   const chatContainerRef = useRef(null);
   const chatInputRef = useRef(null);
 
+  const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am ChatGPT",
+      sender: "ChatGPT"
+    }
+  ]);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing"
+    }
+
+    const newMessages = [...messages, newMessage]; // all the old messages + new message
+
+    // update our message state
+    setMessages(newMessages);
+
+    // set a typing indicator
+    setTyping(true);
+    // process message to chat gpt (send it over and see the response)
+    await processMessageToChatGPT(newMessages);
+  }
+
+  async function processMessageToChatGPT(chatMessages){
+    // chatMessages {sender: "user" or "ChatGPT", message: "The message content here"}
+    // apiMessages ( role: "user" or "assistant", content: "The message content here")
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role ="";
+      if(messageObject.sender === "ChatGPT") {
+        role="assistant"
+      } else{
+        role = "user"
+      }
+      return { role: role, content: messageObject.message}
+    });
+
+    // role: "user" -> a message from the user, "assistant" -> a reponse from GPT
+    // "System" -> generally one initial message defining HOW we want GPT to talk.
+
+    const systemMessage = {
+      role: "system",
+      content: "Explain like a finacial consultant and be very clear about any steps or advice"
+    }
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages":[
+        ...apiMessages // [message 1,message 2, message 3]
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      console.log(data.choices[0].message.content);
+      setMessages(
+        [...chatMessages, {
+          message: data.choices[0].message.content,
+          sender: "ChatGPT"
+        }]
+      );
+      setTyping(false);
+    });
+  }
 
   // Function to process uploaded file
   const processFile = (file) => {
@@ -233,22 +313,19 @@ const Dashboard = () => {
                 <div className="Dash-AI-Title-Con">
                   <p className="ChartTitle">Your Personal Finacial Assistant</p>
                 </div>
-                <div className="chat-container" ref={chatContainerRef}>
-                {messages.map((message, index) => (
-                  <div key={index} className={`chat-message ${message.sender}`}>
-                    <p dangerouslySetInnerHTML={{ __html: message.text.replace(/\n/g, '<br>') }}></p>
-                  </div>
-                ))}
-                </div>
-                <div className='textarea-container'>
-                  <textarea
-                  ref={chatInputRef}
-                    className="chat-input"
-                    placeholder="Type your message..."
-                    onKeyDown={handleNewUserMessage}
-                  />
-                  <button className="send-button" onClick={handleSendClick}><PlaneIcon className="sendIcon"/></button>
-                </div>
+                <MainContainer>
+                  <ChatContainer>
+                    <MessageList
+                      scrollBehavior='smooth'
+                      typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing" /> : null}
+                    >
+                      {messages.map((message, i) => {
+                        return <Message key={i} model={message}/>
+                      })}
+                    </MessageList>
+                    <MessageInput placeholder='Send a message' onSend={handleSend}/>
+                  </ChatContainer>
+                </MainContainer>
               </div>
             </div>
           </div>
